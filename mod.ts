f08b62f2084c143d66e58951f369d62e95e2fa24
vwkd <33468089+vwkd@users.noreply.github.com>
2022-05-20 22:39:55 +0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ function handler(_req: Request) {
   const data = {
     Hello: "World!",
   };
-  return new Response.json(body);
+  return new Response.json(data);
 }
 
 console.log("Listening on http://localhost:8000");
